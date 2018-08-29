48828af0c32d0afa7475676b03c913b73a85020f
C Selvaraju <cselvaraju@gmail.com>
2018-08-29 16:04:00 +0530

    Commit third

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { Post } from '../../models/Post';
+import { Post } from '../models/Post';
 
 const httpOptions = {
   headers: new HttpHeaders(
